46b020e8da574b3399cec8987d2258d2e9b5b2ea
Mac-2015 <mac-2015@Mac-2015.local>
2022-02-06 13:12:40 -0500

    Added users admin instrcutor and student table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :students
+  resources :instructors
+  resources :admins
+  resources :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
