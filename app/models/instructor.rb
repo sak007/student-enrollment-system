bbc2af48b9e53d10a410b6440baffda726dde206
Vishal Veera Reddy <vveerar2@ncsu.edu>
2022-02-07 12:17:00 -0500

    Added Constrinats for instructos

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,9 @@
 class Instructor < ApplicationRecord
   has_secure_password
   has_many :course
+  validates :user_email, presence: true,format: { with: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+  message: "Please Enter a valid email ID" }
+  validates :name, presence: true,
+  validates :password_digest, presence: true,
+  validates :department, presence: true
 end
