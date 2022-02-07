aabf64848a8f976613fa44e55ec03c3a42ae6279
Vishal Veera Reddy <vveerar2@ncsu.edu>
2022-02-07 17:31:43 -0500

    uniq

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,8 +1,13 @@
 class Instructor < ApplicationRecord
   has_secure_password
   has_many :course
+<<<<<<< Updated upstream
   validates :user_email, presence: true, uniqueness: true, format: { with: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
   message: "Please Enter a valid email ID" }
+=======
+  validates :user_email, presence: true,format: { with: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+  message: "Please Enter a valid email ID" },uniqueness: true,
+>>>>>>> Stashed changes
   validates :name, presence: true,
   validates :password_digest, presence: true,
   validates :department, presence: true
