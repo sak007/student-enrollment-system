6d5dc1afd5921f116608e0586b21d7626672e688
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-02-08 15:51:15 -0500

    Fix validations (#16)
    * fix: syntax errors
    
    * fix: email validation

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,9 +1,9 @@
 class Instructor < ApplicationRecord
   has_secure_password
   has_many :course
-  validates :user_email, presence: true, uniqueness: true, format: { with: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+  validates :user_email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP,
   message: "Please Enter a valid email ID" }
-  validates :name, presence: true,
-  validates :password_digest, presence: true,
+  validates :name, presence: true
+  validates :password_digest, presence: true
   validates :department, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,10 @@
 class Student < ApplicationRecord
   has_secure_password
-  validates :user_email, presence: true, uniqueness: true, format: { with: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+  validates :user_email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP,
   message: "Please Enter a valid email ID" }
-  validates :name, presence: true,
-  validates :password_digest, presence: true,
+  validates :name, presence: true
+  validates :password_digest, presence: true
   validates :phone, presence: true, length: { minimum: 10,maximum: 14 }
-  validates :dob, presence: true,
-  validates: :major, presence: true
+  validates :dob, presence: true
+  validates :major, presence: true
 end
