3d57854d2bced24fb26ecd2169a5b67250052144
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-02-10 12:41:55 -0500

    Migrate db to Postgres (#21)
    * setup postgres
    
    * update db user
    
    * delete sqllite db
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # README
 
+## Postgres setup
 
+elecute the following commands
+```
+brew install postgres
+psql postgres #this will get u inside psql
+create database ses_app;
+create role ses_user superuser;
+```
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_02_06_215257) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "user_email"
