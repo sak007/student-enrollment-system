8671369d33e1397b9ff3853d3b3fe7b3200e03f5
Rachana <rkondab@ncsu.edu>
2022-02-15 17:55:03 -0500

    added constraints

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,17 @@
 class Course < ApplicationRecord
     belongs_to :instructor
     has_many :enrollment, dependent: :delete_all
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :instructor_name, presence: true
+    validates :weekday1, presence: true, inclusion: { in: ['MON', 'TUE', 'WED', 'THU', 'FRI'] }
+    validates :weekday2,comparison: {other_than: :weekday1 }, allow_nil: true
+    validates :starttime, presence: true
+    validates :endtime, presence: true
+    #validates :code, presence: true, uniqueness: true, format: { with: \[A-Z]{3}d{3} } message: "Please Enter a valid code" }
+    validates :capacity, presence: true
+    validates :wlcapacity, presence: true
+    validates :room, presence: true
+    validates :status, presence: true
+
 end
