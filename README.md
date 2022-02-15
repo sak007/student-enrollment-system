d301ce7dd3a161116712fc6b801a035994b4aad0
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-02-15 14:05:26 -0500

    Added scripts to seed data (#56)
    * db: add scripts to seed data
    
    * fix: allow course to have 1 weekday
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,16 +1,89 @@
-# README
+# Student Enrollment System
+## Team_2675
+
+## Content
+
+| S.No | Title | Hyperlink |
+| - | - | - |
+| 1 | Deployment Information | [Click here](#deployment-information) |
+| 2 | Credentials | [Click here](#credentials) |
+| 3 | Installation Instructions | [Click here](#installation-instructions) |
+| 4 | Deployment Instructions | [Click here](#deployment-instructions) |
+| 5 | Team Members | [Click here](#team-members) |
+
 
 ## Deployment Information
-
+- Provider: Heroku
 - URL: https://fierce-mesa-20166.herokuapp.com/
 - Admin credentials - admin@ses.com/admin
 
-## Postgres setup
+## Credentials
 
-elecute the following commands
+Apart from the admin, the environment contains the following user data seeded to ease the testing process.
+Login using any of the following credentials.
+- **Note: Since users can delete their account, it is not guaranteed that all these accounts will be available at all the time. We will reset the data regularly to make sure these accounts are available.**
+
+| S.No | Role | User Email | Password | 
+|-|-|-|-|
+| 1. | Admin | admin@ses.com | admin |
+| 2. | Instructor | ron@ses.com | instructor |
+| 3. | Instructor | leslie@ses.com | instructor |
+| 4. | Instructor | chris@ses.com | instructor |
+| 5. | Instructor | ben@ses.com | instructor |
+| 6. | Student | tom@ses.com | student |
+| 7. | Student | andy@ses.com | student |
+| 8. | Student | april@ses.com | student |
+| 9. | Student | donna@ses.com | student |
+| 10. | Student | garry@ses.com | student |
+| 11. | Student | ann@ses.com | student |
+| 12. | Student | craig@ses.com | student |
+
+## Installation Instructions
+
+### Install Ruby and Rails
+  - [Installation Instructions](https://guides.rubyonrails.org/v5.1/getting_started.html)
+
+### Postgres setup
+
+execute the following commands (for macos)
 ```
 brew install postgres
 psql postgres #this will get u inside psql
 create database ses_app;
-create role ses_user superuser;
+create role ses_user superuser login;
 ```
+
+### Setup App
+
+From the project root directory execute the following commands
+
+```
+bundle
+rails db:migrate
+rails db:seed # if u need seeded data
+```
+
+### Run App
+
+Execute 'rails s' to start the application.
+The application can be accessed from localhost:3000
+
+## Deployment Instructions
+
+  - Create a account in [Heroku](https://signup.heroku.com/login)
+  - Install [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli)
+  - From the project root directory run the following commands
+    ```
+    bundle
+    heroku create
+    git push heroku main # git push heroku <branch_name>:main => if you want to push a different branch to heroku main
+    heroku run rake db:migrate
+    heroku run rake db:seed
+    ```
+
+## Team Members
+
+  - Ashok Kumar Selvam - aselvam
+  - Rachana Kondabala - rkondab
+  - Vishal Veera Reddy - vveerar2
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,89 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Clean data before seeding
+Enrollment.delete_all
+Course.delete_all
+Instructor.delete_all
+Student.delete_all
+Admin.delete_all
+User.delete_all
+
+# Admin user
 User.create(email: 'admin@ses.com', role: 'ADMIN', password_digest: BCrypt::Password.create('admin'))
 Admin.create(name: 'Admin', user_email: 'admin@ses.com', phone: 9191919191, password_digest: BCrypt::Password.create('admin'))
+
+# Instructors
+User.create(email: 'ron@ses.com', role: 'INSTRUCTOR', password_digest: BCrypt::Password.create('instructor'))
+Instructor.create(name: 'Ron Swanson', user_email: 'ron@ses.com', password_digest: BCrypt::Password.create('instructor'), department: 'PNR')
+
+User.create(email: 'leslie@ses.com', role: 'INSTRUCTOR', password_digest: BCrypt::Password.create('instructor'))
+Instructor.create(name: 'Leslie Knope', user_email: 'leslie@ses.com', password_digest: BCrypt::Password.create('instructor'), department: 'PNR')
+
+User.create(email: 'chris@ses.com', role: 'INSTRUCTOR', password_digest: BCrypt::Password.create('instructor'))
+Instructor.create(name: 'Chris Pratt', user_email: 'chris@ses.com', password_digest: BCrypt::Password.create('instructor'), department: 'PNR')
+
+User.create(email: 'ben@ses.com', role: 'INSTRUCTOR', password_digest: BCrypt::Password.create('instructor'))
+Instructor.create(name: 'Ben Wyatt', user_email: 'ben@ses.com', password_digest: BCrypt::Password.create('instructor'), department: 'PNR')
+
+# Students
+User.create(email: 'tom@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'Tom Haverford', password_digest: BCrypt::Password.create('student'), dob: '1990-12-01',
+               user_email: 'tom@ses.com', phone: 9876543210, major: 'PNR')
+
+User.create(email: 'andy@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'Andy Dwyer', password_digest: BCrypt::Password.create('student'), dob: '1991-11-02',
+               user_email: 'andy@ses.com', phone: 9876543211, major: 'PNR')
+
+User.create(email: 'april@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'April Ludgate', password_digest: BCrypt::Password.create('student'), dob: '1992-10-03',
+               user_email: 'april@ses.com', phone: 9876543212, major: 'PNR')
+
+User.create(email: 'donna@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'Donna Meagle', password_digest: BCrypt::Password.create('student'), dob: '1993-09-04',
+               user_email: 'donna@ses.com', phone: 9876543213, major: 'PNR')
+
+User.create(email: 'garry@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'Garry Gergich', password_digest: BCrypt::Password.create('student'), dob: '1994-08-05',
+               user_email: 'garry@ses.com', phone: 9876543214, major: 'PNR')
+
+User.create(email: 'ann@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'Ann Perkins', password_digest: BCrypt::Password.create('student'), dob: '1995-07-06',
+               user_email: 'ann@ses.com', phone: 9876543215, major: 'PNR')
+
+User.create(email: 'craig@ses.com', role: 'STUDENT', password_digest: BCrypt::Password.create('student'))
+Student.create(name: 'Craig Middlebrooks', password_digest: BCrypt::Password.create('student'), dob: '1994-06-07',
+               user_email: 'craig@ses.com', phone: 9876543216, major: 'PNR')
+
+# Courses
+Course.create(name: 'Time Management', description: 'Time Management', weekday1: 'MON', weekday2: 'WED',
+              starttime: '11:45', endtime: '13:00', code: 'MAN121', capacity: 2, wlcapacity: 1, status: 'OPEN',
+              room: 'R101', instructor_id: Instructor.find_by_user_email('leslie@ses.com').id)
+
+# Course.create(name: '', description: '', weekday1: '', weekday2: '',
+#               starttime: '', endtime: '', code: 'PNR', capacity: 2, wlcapacity: 1, status: 'OPEN',
+#               room: 'R', instructor_id: Instructor.find_by_user_email('leslie@ses.com').id)
+
+Course.create(name: 'Hunting', description: 'Hunting', weekday1: 'MON', weekday2: 'WED',
+              starttime: '11:00', endtime: '12:00', code: 'REC101', capacity: 2, wlcapacity: 1, status: 'OPEN',
+              room: 'The Forest', instructor_id: Instructor.find_by_user_email('ron@ses.com').id)
+
+Course.create(name: 'Fishing', description: 'Fishing', weekday1: 'TUE', weekday2: 'THU',
+              starttime: '8:30', endtime: '9:45', code: 'REC102', capacity: 2, wlcapacity: 1, status: 'OPEN',
+              room: 'The Lake', instructor_id: Instructor.find_by_user_email('ron@ses.com').id)
+
+Course.create(name: 'Management Basics', description: 'Management Basics', weekday1: 'TUE', weekday2: 'THU',
+              starttime: '8:00', endtime: '9:15', code: 'MAN101', capacity: 2, wlcapacity: 1, status: 'OPEN',
+              room: 'R102',  instructor_id: Instructor.find_by_user_email('chris@ses.com').id)
+
+Course.create(name: 'Anger Management', description: 'Anger Management', weekday1: 'MON', weekday2: 'WED',
+              starttime: '8:00', endtime: '9:15', code: 'PYC121', capacity: 2, wlcapacity: 1, status: 'OPEN',
+              room: 'R104', instructor_id: Instructor.find_by_user_email('chris@ses.com').id)
+
+Course.create(name: 'Politics', description: 'Politics', weekday1: 'FRI',
+              starttime: '3:00', endtime: '5:00', code: 'POL101', capacity: 3, wlcapacity: 1, status: 'OPEN',
+              room: 'R103', instructor_id: Instructor.find_by_user_email('ben@ses.com').id)
+
+
+# Enrollments
