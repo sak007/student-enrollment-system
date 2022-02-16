dfa71b3ac35872cc02d4c2c6469478656bddc688
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-02-15 19:05:31 -0500

    Constraints for courses (#59)
    * added constraints
    
    * fix: fix constraints
    
    Co-authored-by: Rachana <rkondab@ncsu.edu>

--- db/migrate/20220206223858_add_keys_to_courses.rb
+++ db/migrate/20220206223858_add_keys_to_courses.rb
@@ -1,6 +1,7 @@
 class AddKeysToCourses < ActiveRecord::Migration[7.0]
 def change
-    add_reference :courses, :instructor, null: false, foreign_key: true
+
+  add_reference :courses, :instructor, null: false, foreign_key: true
   end
 
 end
