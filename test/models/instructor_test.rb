03ea452613c63ac1031301a0de949980470be9f4
Vishal Veera Reddy <vveerar2@ncsu.edu>
2022-02-21 23:07:56 -0500

    added patch

--- test/models/instructor_test.rb
+++ test/models/instructor_test.rb
@@ -1,7 +1,56 @@
 require "test_helper"
 
 class InstructorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @instructor = instructors(:one)
+  end
+
+  test 'valid instructor' do
+    instructor = @instructor
+    assert instructor.valid?
+  end
+
+  test 'invalid without name' do
+    instructor = @instructor
+    instructor.name = nil
+    refute instructor.valid?
+    assert_not_nil instructor.errors[:name]
+  end
+
+  test 'invalid without email' do
+    instructor = @instructor
+    instructor.user_email = nil
+    refute instructor.valid?
+    assert_not_nil instructor.errors[:user_email]
+  end
+
+  test 'invalid email without prefix' do
+    instructor = @instructor
+    instructor.user_email = '@a.com'
+    refute instructor.valid?
+    assert_not_nil instructor.errors[:user_email]
+  end
+
+  test 'invalid email without sufix' do
+    instructor = @instructor
+    instructor.user_email = 'a@.com'
+    refute instructor.valid?
+    assert_not_nil instructor.errors[:user_email]
+  end
+
+  test 'invalid email without @' do
+    instructor = @instructor
+    instructor.user_email = 'a.com'
+    refute instructor.valid?
+    assert_not_nil instructor.errors[:user_email]
+  end
+
+  test 'invalid without department' do
+    instructor = @instructor
+    instructor.department = nil
+    refute instructor.valid?
+    assert_not_nil instructor.errors[:department]
+  end
+
+
 end
